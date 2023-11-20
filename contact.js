820a3a5d586045083f565a53ac78636edc3a9674
Reddi DIleepKumar <78788832+dileep011@users.noreply.github.com>
2023-11-20 20:12:55 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,33 @@
+function openMenu() {
+  var navMain = document.getElementById("navMenuId");
+  if (navMain.className === "navMenu") {
+    navMain.className = " mobileView";
+  } else {
+    navMain.className = "navMenu";
+  }
+}
+jQuery(document).ready(function ($) {
+	"use strict";
+	//  TESTIMONIALS CAROUSEL HOOK
+	$("#customers-testimonials").owlCarousel({
+		loop: true,
+		center: true,
+		items: 3,
+		margin: 0,
+		autoplay: true,
+		dots: true,
+		autoplayTimeout: 8500,
+		smartSpeed: 450,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			1170: {
+				items: 3
+			}
+		}
+	});
+});
